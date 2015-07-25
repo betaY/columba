46f8ebce2a046852257b898237527517c0cdf10f
Junyu <beta168921@gmail.com>
2015-07-24 21:11:30 -0400

    login view controller

---
+++ Columba/LoginViewController.swift
@@ -0,0 +1,57 @@
+//
+//  LoginViewController.swift
+//  Columba
+//
+//  Created by Beta on 15/7/23.
+//  Copyright © 2015年 Beta. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var userEmailTextField: UITextField!
+    @IBOutlet weak var userPasswordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loginButtonTapped(sender: AnyObject) {
+        
+        let userEmail = userEmailTextField.text
+        let userPassword = userPasswordTextField.text
+        
+        //check userEmail and Password
+        let userEmailStored = NSUserDefaults.standardUserDefaults().stringForKey("userEmail")
+        let userPasswordStored = NSUserDefaults.standardUserDefaults().stringForKey("userPassword")
+        if (userEmailStored == userEmail) {
+            if (userPasswordStored == userPassword) {
+                // Login is successful
+                
+                NSUserDefaults.standardUserDefaults().setBool(true, forKey: "isUserLoggedIn")
+                NSUserDefaults.standardUserDefaults().synchronize()
+                self.dismissViewControllerAnimated(true, completion: nil)
+            }
+        }
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
