15ab5c3dc5787065a956bfb474d1c634797c4cbd
Junyu <beta168921@gmail.com>
2015-07-24 21:12:35 -0400

    auto generate

---
+++ ColumbaUITests/ColumbaUITests.swift
@@ -0,0 +1,34 @@
+//
+//  ColumbaUITests.swift
+//  ColumbaUITests
+//
+//  Created by Beta on 15/7/23.
+//  Copyright © 2015年 Beta. All rights reserved.
+//
+
+import XCTest
+
+class ColumbaUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
