07807cc99cd6935b425f4d4a206ec43c398a53be
Junyu <beta168921@gmail.com>
2015-07-25 21:02:23 -0400

    database setting

---
+++ Columba/Conn.php
@@ -0,0 +1,8 @@
+<?php
+class Conn {
+    public static $dbhost = "beta.moe";
+    public static $dbuser = "yudachi";
+    public static $dbpass = "poi";
+    public static $dbname = "columba";
+}
+?>
